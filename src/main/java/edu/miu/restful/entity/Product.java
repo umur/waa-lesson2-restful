d8998a9f1e28204650cd8c0ae038b57bcb2a2eff
umur inan <umurinan@gmail.com>
2022-03-17 21:27:32 -0500

    delete product bug fix

--- src/main/java/edu/miu/restful/entity/Product.java
+++ src/main/java/edu/miu/restful/entity/Product.java
@@ -2,10 +2,13 @@ package edu.miu.restful.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Product {
 
